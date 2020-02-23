7c42815d431c212ec67fe23e59b1199e732315a2
thoma <thoma@DESKTOP-6L438FJ.hsd1.ca.comcast.net>
2020-02-23 00:01:08 -0800

    I have added java.util.*; to allow access to scanner. Added questions

--- Hello World/src/Hello_Simple.java
+++ Hello World/src/Hello_Simple.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Hello_Simple {
 
@@ -5,6 +6,28 @@ public class Hello_Simple {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World");
+		
+		Scanner kbd = new Scanner(System.in);
+		
+		System.out.print("What is your name: ");
+		
+		String name;
+		
+		name = kbd.next();
+		
+		System.out.print("Hello " + name + " how are you doing today from 1 to 5: ");
+		
+		int mood;
+		
+		mood = kbd.nextInt();
+		
+		if (mood > 3) {
+			System.out.println("It seems that your having a average day or above");
+			
+		}
+		else {
+			System.out.println("Why are you upset?");
+		}
 	}
 
 }
