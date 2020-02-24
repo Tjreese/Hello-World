e76499a551167bff95c826805b6094f4a9e67d76
thoma <thoma@DESKTOP-6L438FJ.hsd1.ca.comcast.net>
2020-02-23 22:17:17 -0800

    I fixed the feedback array I will clean up code soon and shorten it

--- Hello World/src/Hello_Simple.java
+++ Hello World/src/Hello_Simple.java
@@ -4,29 +4,42 @@ public class Hello_Simple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		System.out.println("Hello World");
-		
+
 		Scanner kbd = new Scanner(System.in);
-		
+
 		System.out.print("What is your name: ");
-		
+
 		String name;
-		
+
 		name = kbd.next();
-		
+
 		System.out.print("Hello " + name + " how are you doing today from 1 to 5: ");
-		
+
 		int mood;
-		
+
 		mood = kbd.nextInt();
-		
+
 		if (mood > 3) {
 			System.out.println("It seems that your having a average day or above");
-			
-		}
-		else {
-			System.out.println("Why are you upset?");
+
+		} else {
+			Scanner kbd1 = new Scanner(System.in);
+			System.out.println("Why are you upset?: ");
+			String upsetReason;
+			upsetReason = kbd1.nextLine();
+			int Reasoncounter = 0; 
+			 Reasoncounter = Reasoncounter + 1;			
+			String[] feedBack = new String[Reasoncounter];
+			if (upsetReason != "") {
+				for (int i = 0; i < Reasoncounter; i++) {
+					feedBack[i] = upsetReason;
+					System.out.println("The first thing that can help is " + feedBack[i] + " according to you");
+				}
+			}
+
+			System.out.println("Thank you for the feed back I will see what we can do to improve");
+
 		}
 	}
 
